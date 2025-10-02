fc33e2c2a74e2693acfdb01def7af3660d72832d
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-10-02 15:02:06 +0200

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,16 @@
+#reverse string using two pointer method
+
+def reverse_string(s):
+    chars = list(s)
+    left, right = 0, len(chars) - 1
+    while left < right:
+        chars[left], chars[right] = chars[right], chars[left]
+        left += 1
+        right -= 1
+
+    return "".join(chars)
+
+#Example
+print(reverse_string('Hello'))
+print(reverse_string('Python'))
+
